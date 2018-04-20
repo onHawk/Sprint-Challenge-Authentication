3c1533becb8074d80590810e53f133e2c0008547
unknown <luisd824@gmail.com>
2018-04-20 13:11:29 -0400

    finished base requirenments

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -4,24 +4,50 @@ const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 11;
 
-const UserSchema = Schema({
+const UserSchema = new mongoose.Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 UserSchema.pre('save', function(next) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
+  // Fill this middleware in with the Proper password encrypting,
+  bcrypt
+    .hash(this.password, 5)
+    .then(hash => {
+      this.password = hash;
+
+      next();
+    })
+    .catch(err => {
+      console.log('error', err);
+    });
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
 });
 
+// https://github.com/kelektiv/node.bcrypt.js#usage
+// Fill this method in with the Proper password comparing, bcrypt.compare()
+// Your controller will be responsible for sending the information here for password comparison
+// Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this method in with the Proper password comparing, bcrypt.compare()
-  // Your controller will be responsible for sending the information here for password comparison
-  // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, (err, isValid) => {
+    if (err) {
+      return callBack(err);
+    }
+    callBack(null, isValid);
+  });
 };
 
 module.exports = mongoose.model('User', UserSchema);
--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/auth-users', {
-  useMongoClient: true
+mongoose.connect('mongodb://localhost/auth-users').then(() => {
+  console.log('connected to mongo');
 });
 
 server.listen(port, () => {
